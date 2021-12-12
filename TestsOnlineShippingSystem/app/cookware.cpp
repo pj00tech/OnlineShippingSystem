bee0845c0750365abfbf6342384b6d339682b40c
pj00tech <92899974+pj00tech@users.noreply.github.com>
2021-12-11 18:18:20 -0600

    Update cookware.cpp

--- TestsOnlineShippingSystem/app/cookware.cpp
+++ TestsOnlineShippingSystem/app/cookware.cpp
@@ -1,15 +1,22 @@
+// Incllude headear file for cookware
 #include "cookware.h"
 
+//Default Constructor
 Cookware::Cookware() {
 
 }
+
+//Parametrized Constructor to take the number of cookware sets as pramenter.
 Cookware::Cookware(string sets) {
 	numberOfCookwareSets = sets;
 }
 
+//Function to set the number of cookware sets.
 void Cookware::setNumberOfCookwareSets(string sets) {
 	numberOfCookwareSets = sets;
 }
+
+//Function to return the number of cookware sets.
 string Cookware::GetNumberOfCookwareSets() {
 
 	return numberOfCookwareSets;
