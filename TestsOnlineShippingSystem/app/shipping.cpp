93dbb0fb1bafe3a39aa8573eededfb613f6d87fb
pj00tech <92899974+pj00tech@users.noreply.github.com>
2021-12-11 18:34:38 -0600

    Update shipping.cpp

--- TestsOnlineShippingSystem/app/shipping.cpp
+++ TestsOnlineShippingSystem/app/shipping.cpp
@@ -1,18 +1,23 @@
+//Include header file for shipping
 #include "shipping.h"
 
-
+//Default consturctor
 Shipping::Shipping() {
 
 }
 
+//Parametrized consturctor to take the shipping priority as prameter.
 Shipping::Shipping(string priority) {
 	shippingPriority = priority;
 }
 
+//Funtion to set the shipping priority
 void Shipping::setShippingChoice(string priority) {
 	shippingPriority = priority;
 
 }
+
+//Funtion to return the shipping priority
 string Shipping::getShippingChoice() {
 	return shippingPriority;
 }
