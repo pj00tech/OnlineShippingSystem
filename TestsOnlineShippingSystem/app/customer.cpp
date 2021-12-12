54e2150eec9dacd43b2be25d870160941fccb623
pj00tech <92899974+pj00tech@users.noreply.github.com>
2021-12-11 20:03:58 -0600

    Update customer.cpp

--- TestsOnlineShippingSystem/app/customer.cpp
+++ TestsOnlineShippingSystem/app/customer.cpp
@@ -1,33 +1,33 @@
-//Include header file for customer
+// Include header file for customer.
 #include "customer.h"
 
-//Default consturctor
+// Default constructor.
 Customer::Customer() {
 
 }
 
-//Parametrized consturctor to take the customer ID and name as pramerters
+// Parameterized constructor to take the customer ID and name as parameters.
 Customer::Customer(string ID, string name) {
 	customerID = ID;
 	customerName = name;
 }
 
-//Function to set the customer name
+// Function to set the customer name.
 void Customer::setCustomerName(string name) {
 	customerName = name;
 }
 
-//Function to return the customer name
+// Function to return the customer name.
 string Customer::getCustomerName() {
 	return customerName;
 }
 
-//Function to set the customer ID
+// Function to set the customer ID.
 void Customer::setCustomerID(string ID) {
 	customerID = ID;
 }
 
-//Function to retrun the customer ID
+// Function to retrun the customer ID.
 string Customer::getCustomerID() {
 	return customerID;
 }
