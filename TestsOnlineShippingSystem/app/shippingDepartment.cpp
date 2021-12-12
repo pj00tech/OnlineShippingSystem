6b162076c438747915a961933f7662988d295b98
pj00tech <92899974+pj00tech@users.noreply.github.com>
2021-12-11 20:09:26 -0600

    Update shippingDepartment.cpp

--- TestsOnlineShippingSystem/app/shippingDepartment.cpp
+++ TestsOnlineShippingSystem/app/shippingDepartment.cpp
@@ -1,12 +1,12 @@
-//Include header file for shipping
+// Include header file for shipping.
 #include "shippingDepartment.h"
 
-//Default Constructor
+// Default constructor.
 ShippingDepartment::ShippingDepartment() {
 	
 }
 
-//Parametrized Consturctor to take order number, customer ID and name, number of cookware sets and shipping priority as prameter
+// Parameterized constructor to take order number, customer ID and name, number of cookware sets, and shipping priority as parameters.
 ShippingDepartment::ShippingDepartment(string order, string ID, string name, string sets, string priority) {
 	orderID = order;
 	customerID=ID;
@@ -15,17 +15,17 @@ ShippingDepartment::ShippingDepartment(string order, string ID, string name, str
 	shippingPriority = priority;
 }
 
-///Parametrized Consturctor to take order number as parameter
+// Parameterized constructor to take order number as parameter.
 ShippingDepartment::ShippingDepartment(string order) {
 	orderID = order;
 }
 
-//Function to set the order number
+// Function to set the order number.
 void ShippingDepartment::setOrderID(string ID) {
 	orderID = ID;
 }
 
-//Function to return the order number
+// Function to return the order number.
 string ShippingDepartment::getOrderID() {
 	return orderID;
 }
